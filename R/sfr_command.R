95d6df1201c1ca29271f8e62c05af6d84467bd3d
Leszek <32634801+Leszek-Sieminski@users.noreply.github.com>
2019-07-13 16:31:12 +0200

    create setup, crawl & command functions

---
+++ R/sfr_command.R
@@ -0,0 +1,145 @@
+
+sfr_command <- function(
+  url,
+  output_folder             = NULL,
+  timestamped_output        = NULL,
+  format                    = "csv", # "csv" "xls" "xlsx"
+  save_crawl_file           = TRUE,
+  export_tabs               = NULL,
+  export_bulk               = NULL,
+  export_report             = NULL,
+  overwrite                 = TRUE,
+  headless                  = TRUE,
+  config                    = NULL,
+  create_sitemap            = FALSE,
+  create_images_sitemap     = FALSE,
+  use_majestic              = NULL,
+  use_mozscape              = NULL,
+  use_ahrefs                = NULL,
+  use_google_analytics      = NULL,
+  use_google_search_console = NULL)
+{
+  # commands preparation ------------------------------------------------------
+  c <- list()
+
+
+  # commands preparation: url -------------------------------------------------
+  if (length(url) > 1) {
+    c$crawl_com <- glue("--crawl-list {paste(url, collapse = ' ')}")
+  } else {
+    c$crawl_com <- glue("--crawl {url}")
+  }
+
+
+  # commands preparation: output folder ---------------------------------------
+  if (!is.null(output_folder)) {
+    c$output_folder_com <- glue("--output-folder {output_folder}")
+  } else {
+    output_folder <- paste0(getwd(), "/")
+    c$output_folder_com <- glue("--output-folder {output_folder}")
+  }
+
+
+  # commands preparation: timestamped_output ----------------------------------
+  if (timestamped_output) {
+    c$timestamped_output_com <- "--timestamped-output"
+  } else {
+    c$timestamped_output_com <- NULL
+  }
+
+
+  # commands preparation: format ----------------------------------------------
+  if (!is.null(format)) {
+    c$format_com <- glue("--export-format {format}")
+  } else {
+    c$format_com <- NULL
+  }
+
+
+  # commands preparation: save_crawl_file -------------------------------------
+  if (save_crawl_file) {
+    c$save_crawl_file_com <- "--save-crawl"
+  } else {
+    c$save_crawl_file_com <- NULL
+  }
+
+
+  # commands preparation: export_tabs -----------------------------------------
+  if (!is.null(export_tabs)) {
+    c$export_tabs_com <- "--export-tabs '{paste(export_tabs, collapse = ',')}'"
+  } else {
+    c$export_tabs_com <- NULL
+  }
+
+
+  # commands preparation: export_bulk -----------------------------------------
+  if (!is.null(export_bulk)) {
+    c$export_bulk_com <- "--bulk-export '{paste(export_bulk, collapse = ',')}'"
+  } else {
+    c$export_bulk_com <- NULL
+  }
+
+  # commands preparation: export_report ---------------------------------------
+  if (!is.null(export_report)) {
+    c$export_report_com <- "--save-report '{paste(export_report, collapse = ',')}'"
+  } else {
+    c$export_report_com <- NULL
+  }
+
+  # commands preparation: overwrite -------------------------------------------
+  if (overwrite) {
+    c$overwrite_com <- "--overwrite"
+  } else {
+    c$overwrite_com <- NULL
+  }
+
+  # commands preparation: headless --------------------------------------------
+  if (headless) {
+    c$headless_com <- "--headless"
+  } else {
+    c$headless_com <- NULL
+  }
+
+  # commands preparation: config ----------------------------------------------
+  if (!is.null(config)) {
+    c$config_com <- glue("--config {config}")
+  } else {
+    c$config_com <- NULL
+  }
+
+  # commands preparation: create_sitemap --------------------------------------
+  if (!is.null(create_sitemap)) {
+    c$create_sitemap_com <- "--create-sitemap"
+  } else {
+    c$create_sitemap_com <- NULL
+  }
+
+  # commands preparation: create_images_sitemap -------------------------------
+  if (!is.null(create_images_sitemap)) {
+    c$create_images_sitemap_com <- "--create-images-sitemap"
+  } else {
+    c$create_images_sitemap_com <- NULL
+  }
+
+  # commands preparation: use_majestic ----------------------------------------
+  # ?
+
+  # commands preparation: use_mozscape ----------------------------------------
+  # ?
+
+  # commands preparation: use_ahrefs ------------------------------------------
+  # ?
+
+  # commands preparation: use_google_analytics --------------------------------
+  # ?
+
+  # commands preparation: use_google_search_console ---------------------------
+  # ?
+
+  # commands joining ----------------------------------------------------------
+  x <- paste(unlist(c), collapse = " ")
+
+
+  # return --------------------------------------------------------------------
+  return(x)
+}
---
+++ R/sfr_crawl.R
@@ -0,0 +1,151 @@
+#' Title
+#'
+#' @param url
+#' @param output_folder
+#' @param timestamped_output
+#' @param format
+#' @param export_tabs
+#' @param export_bulk
+#' @param export_report
+#' @param overwrite
+#' @param headless
+#' @param config
+#' @param create_sitemap
+#' @param create_images_sitemap
+#' @param use_majestic
+#' @param use_mozscape
+#' @param use_ahrefs
+#' @param use_google_analytics
+#' @param use_google_search_console
+#'
+#' @return
+#' @export
+#'
+#' @importFrom glue glue
+#'
+#' @examples
+sfr_crawl <- function(
+  url,
+  output_folder             = NULL,
+  timestamped_output        = NULL,
+  format                    = "csv", # "csv" "xls" "xlsx"
+  save_crawl_file           = TRUE,
+  export_tabs               = NULL,
+  export_bulk               = NULL,
+  export_report             = NULL,
+  overwrite                 = TRUE,
+  headless                  = TRUE,
+  config                    = NULL,
+  create_sitemap            = NULL,
+  create_images_sitemap     = NULL,
+  use_majestic              = NULL,
+  use_mozscape              = NULL,
+  use_ahrefs                = NULL,
+  use_google_analytics      = NULL,
+  use_google_search_console = NULL
+) {
+  # safety net ----------------------------------------------------------------
+
+
+  # building command ----------------------------------------------------------
+  command <- sfr_command(
+    url                       = url,
+    output_folder             = output_folder,
+    timestamped_output        = timestamped_output,
+    format                    = format,
+    save_crawl_file           = save_crawl_file,
+    export_tabs               = export_tabs,
+    export_bulk               = export_bulk,
+    export_report             = export_report,
+    overwrite                 = overwrite,
+    headless                  = headless,
+    config                    = config,
+    create_sitemap            = create_sitemap,
+    create_images_sitemap     = create_images_sitemap,
+    use_majestic              = use_majestic,
+    use_mozscape              = use_mozscape,
+    use_ahrefs                = use_ahrefs,
+    use_google_analytics      = use_google_analytics,
+    use_google_search_console = use_google_search_console)
+
+  # execution -----------------------------------------------------------------
+  # command <- glue(
+  #   'cd {Sys.getenv("S_FROG_PATH")}
+  # ScreamingFrogSEOSpiderCli.exe --crawl {url} --headless --output-folder {output}')
+
+
+  if (grepl("Windows", Sys.info()['sysname']) {
+    system2("cmd", input = command)
+  } else {
+    system(command)
+  }
+
+
+
+}
+
+# --crawl <url>
+#   Start crawling the supplied URL
+#
+# --crawl-list <list file>
+#   Start crawling the specified URLs in list mode
+#
+# --config <config>
+#   Supply a config file for the spider to use
+#
+# --use-majestic
+# Use Majestic API during crawl
+#
+# --use-mozscape
+# Use Mozscape API during crawl
+#
+# --use-ahrefs
+# Use Ahrefs API during crawl
+#
+# --use-google-analytics <google account> <account> <property> <view> <segment>
+#   Use Google Analytics API during crawl
+#
+# --use-google-search-console <google account> <website>
+#   Use Google Search Console API during crawl
+#
+# --headless
+# Run in silent mode without a user interface
+#
+# --output-folder <output>
+#   Where to store saved files. Default: current working directory
+#
+# --export-format <csv|xls|xlsx>
+#   Supply a format to be used for all exports
+#
+# --overwrite
+# Overwrite files in output directory
+#
+# --timestamped-output
+# Create a timestamped folder in the output directory, and store
+# all output there
+#
+# --save-crawl
+# Save the completed crawl
+#
+# --export-tabs <tab:filter,...>
+#   Supply a comma separated list of tabs to export. You need to
+# specify the tab name and the filter name separated by a colon
+#
+# --bulk-export <[submenu:]export,...>
+#   Supply a comma separated list of bulk exports to perform. The
+# export names are the same as in the Bulk Export menu in the UI.
+# To access exports in a submenu, use <submenu-name:export-name>
+#
+#   --save-report <[submenu:]report,...>
+#   Supply a comma separated list of reports to save. The report
+# names are the same as in the Report menu in the UI. To access
+# reports in a submenu, use <submenu-name:report-name>
+#
+#   --create-sitemap
+# Creates a sitemap from the completed crawl
+#
+# --create-images-sitemap
+# Creates an images sitemap from the completed crawl
+#
+# -h, --help
+# Print this message and exit
